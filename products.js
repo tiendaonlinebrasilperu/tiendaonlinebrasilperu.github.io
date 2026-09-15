89fb4d2d0784448a1e966a1cb7070a60d62ff1ba
tiendaonlinebrasilperu <arlington.marin@unmsm.edu.pe>
2026-09-14 19:15:14 -0500

    Update products.js

--- products.js
+++ products.js
@@ -48,6 +48,17 @@ const PRODUCTS = [
     price: null,
     image: "https://tiendaonlinebrasilperu.github.io/assets/oleodemanga.jpg",
     badge: "HIDRATACIÓN"
+  },
+  {
+    id: 6,
+    brand: "Skala Brasil",
+    name: "Potão do Amor – Creme de Tratamento + Creme de Pentear 2 em 1",
+    size: "1 kg",
+    category: "Cabello",
+    price: null,
+    image: "https://tiendaonlinebrasilperu.github.io/assets/potaoamor.jpg",
+    badge: "NOVEDAD",
+    description: "Tratamiento 2 en 1 ideal para cabellos dañados o resecos. Su fórmula con aceite de ricino, aceite de argán y queratina vegetal ayuda a nutrir, hidratar y fortalecer la fibra capilar, dejando el cabello suave, brillante y con menos frizz. 100% vegano y libre de sulfatos, parabenos, siliconas, petrolatos y aceites minerales."
   }
 ];
 
